3b3aff0d89925c3859788e8e986af8fa13b88514
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2020-06-23 21:14:38 -0500

    dog animation

--- SpriteKit Demo/GameScene.swift
+++ SpriteKit Demo/GameScene.swift
@@ -11,74 +11,28 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    let myFirstNode = SKNode()
-    let myFirstSpriteNode = SKSpriteNode(color: .red, size: CGSize(width: 200.0, height: 200.00))
-    let myFirstTexturedSpriteNode = SKSpriteNode(imageNamed: "Spaceship")
-    let blueBox = SKSpriteNode(color: .blue, size: CGSize(width: 100.0, height: 100.00))
+    let dogSpriteNode = SKSpriteNode(imageNamed: "Run0")
+    var dogFrames = [SKTexture]()
     
     override func didMove(to view: SKView) {
-        addChild(myFirstNode)
-        myFirstSpriteNode.position = CGPoint(x: frame.midX, y: frame.midY)
-        myFirstSpriteNode.anchorPoint = .zero
-        addChild(myFirstSpriteNode)
+        dogSpriteNode.position = CGPoint(x: frame.midX, y: frame.midY)
+        addChild(dogSpriteNode)
+        let textureAtlas = SKTextureAtlas(named: "Dog Frames")
         
-        myFirstTexturedSpriteNode.size = CGSize(width: 100.0, height: 100.0)
-        
-        myFirstTexturedSpriteNode.zPosition = 1
-        blueBox.zPosition = 2
-        
-        blueBox.position = CGPoint()
-        myFirstSpriteNode.addChild(myFirstTexturedSpriteNode)
-        blueBox.position = CGPoint(x: myFirstSpriteNode.size.width / 2, y: myFirstSpriteNode.size.height / 2)
-        myFirstSpriteNode.addChild(blueBox)
-        
-        physicsWorld.gravity = CGVector(dx: -1.0, dy: -2.0)
-        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
-        
-        myFirstTexturedSpriteNode.physicsBody = SKPhysicsBody(circleOfRadius: myFirstTexturedSpriteNode.size.width/2)
-        myFirstTexturedSpriteNode.physicsBody!.allowsRotation = false
-        myFirstTexturedSpriteNode.physicsBody!.restitution = 1.0
-        
-        
-        blueBox.physicsBody = SKPhysicsBody(rectangleOf: blueBox.size)
-//        blueBox.physicsBody!.allowsRotation = false
-        blueBox.physicsBody!.restitution = 1.0
+        for index in 0..<textureAtlas.textureNames.count {
+            let textureName = "Run" + String(index)
+            dogFrames.append(textureAtlas.textureNamed(textureName))
+        }
         
     }
-    
-    
     
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        
-////        myFirstTexturedSpriteNode.run(SKAction.move(to: CGPoint(x: myFirstSpriteNode.size.width, y: myFirstSpriteNode.size.height), duration: 2.0))
-//        myFirstTexturedSpriteNode.run(SKAction.move(to: CGPoint(x: myFirstSpriteNode.size.width, y: myFirstSpriteNode.size.height), duration: 2.0)) {
-//            self.myFirstTexturedSpriteNode.position = .zero
-//        }
-////        blueBox.run(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 2.0))
-//        if !blueBox.hasActions() {
-////            blueBox.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 2.0)))
-////            blueBox.run(SKAction.group([
-////                SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 2.0),
-////                SKAction.scale(by: 0.9, duration: 2.0)
-////            ]))
-//            blueBox.run(SKAction.sequence([
-//                SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 2.0),
-//                    SKAction.scale(by: 0.9, duration: 2.0)]))
-//        } else {
-//            blueBox.removeAllActions()
-//        }
-        
-//        let actionKey = "Rotation"
-//        if let _ = myFirstTexturedSpriteNode.action(forKey: actionKey) {
-//            myFirstTexturedSpriteNode.removeAction(forKey: actionKey)
-//        } else {
-//            myFirstTexturedSpriteNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 2.0)), withKey: actionKey)
-//        }
-        
-        
+        if !dogSpriteNode.hasActions() {
+          dogSpriteNode.run(SKAction.repeatForever(SKAction.animate(with: dogFrames, timePerFrame: 0.1)))
+        } else {
+            dogSpriteNode.removeAllActions()
+        }
     }
-    
     
 }

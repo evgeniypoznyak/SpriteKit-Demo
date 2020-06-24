35576dd354c9ec96d7c0178e0ed653902ea0b8a8
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2020-06-23 23:59:41 -0500

    tile maps

--- SpriteKit Demo/GameViewController.swift
+++ SpriteKit Demo/GameViewController.swift
@@ -17,18 +17,21 @@ class GameViewController: UIViewController {
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            let scene = GameScene(size: view.bounds.size)
-            // Set the scale mode to scale to fit the window
-            scene.scaleMode = .aspectFill
+//            let scene = GameScene(size: view.bounds.size)
+            if let scene = SKScene(fileNamed: "MyFirstTileMapScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+                
+                view.ignoresSiblingOrder = true
+                
+                view.showsFPS = true
+                view.showsNodeCount = true
+                view.showsPhysics = true
+            }
             
-            // Present the scene
-            view.presentScene(scene)
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-            view.showsPhysics = true
         }
     }
     
